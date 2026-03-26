f4b839e540783b9432eb100622e2fd278201c9bc
Namratha-shankar <1nt23is139.namratha@nmit.ac.in>
2026-03-27 00:42:57 +0530

    success

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -6,13 +6,13 @@ export const addApplication = async (req, res) => {
 
   try {
     await db.query(
-      "INSERT INTO applications (job_id, user_id, resume) VALUES (?, ?, ?)",
+      "INSERT INTO applications (job_id, user_id, resume) VALUES ($1, $2, $3)",
       [job_id, user_id, resume]
     );
 
     res.json({ message: "Application submitted" });
   } catch (err) {
-    console.log(err);
+    console.log("ERROR:", err);
     res.status(500).json({ error: "Server error" });
   }
 };
